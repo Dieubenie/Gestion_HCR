011e4ce5550b42d7ee8a1380f6ff508bec14d564
Dieubénie <kodjim15@gmail.com>
2022-05-25 14:25:21 +0200

    detail

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,7 +1,26 @@
 @extends('layouts.main')
 @section('contenu')
 <h1 class="text-center">Détails du personnel {{$finds->id}} </h1>
-
+<div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="{{ asset('storage').'/'.$finds->photo }}" class="img-fluid rounded-start" alt=""> <br> <br>
+        <img src="{{ asset('img/code.jpg') }}" width="100%"  alt="">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h3 class="card-title text-center text-dangeradd">Carte personnel HCR</h3>
+          <p>Nom: <b>{{$finds->nom}} </b></p>
+          <p>Prénom:<b>{{$finds->prenom}} </b></p>
+          <p>Genre: <b>{{$finds->genre}} </b></p>
+          <p>Poste: <b>{{$finds->poste}} </b></p>
+          <p>Titre: <b>{{$finds->titre}} </b></p>
+          <p>N° matricule: <b>{{$finds->matricule}} </b></p>
+          </h5>
+        </div>
+      </div>
+    </div>
+  </div>
 
 @endsection
 
